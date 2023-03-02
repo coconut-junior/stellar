775b29e5bd44c06078640f9c9718c1b34e39bcf0
Jimmy Blanck <blanckjm@gmail.com>
2023-03-01 19:56:01 -0500

    resized buttons

--- database.js
+++ database.js
@@ -1,18 +1,18 @@
-var mysql = require('mysql');
+// var mysql = require('mysql');
 
-var con = mysql.createConnection({
-  host: "4c4.h.filess.io",
-  user: "productdb_centdegree",
-  port: "3307",
-  password: "95d10fcb2836c5f34740906b4c76474ac270b0a5",
-  database: "productdb_centdegree"
-});
+// var con = mysql.createConnection({
+//   host: "4c4.h.filess.io",
+//   user: "productdb_centdegree",
+//   port: "3307",
+//   password: "95d10fcb2836c5f34740906b4c76474ac270b0a5",
+//   database: "productdb_centdegree"
+// });
 
-con.connect(function(err) {
-  if (err) throw err;
-  var sql = "SHOW TABLES";
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-        console.log(result);
-  });
-}); 
+// con.connect(function(err) {
+//   if (err) throw err;
+//   var sql = "SHOW TABLES";
+//   con.query(sql, function (err, result) {
+//     if (err) throw err;
+//         console.log(result);
+//   });
+// }); 
