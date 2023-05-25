9949cbebb826972f4e302c6fdc7c4019b64c1347
Jimmy Blanck <blanckjm@gmail.com>
2023-05-24 22:03:05 -0400

    implemented resource monitor

---
+++ resource_monitoring.js
@@ -0,0 +1,54 @@
+const { getCPUUsage, cpuUsage } = require("process");
+const { clearInterval } = require("timers");
+const os = require("os");
+
+var startMeasure = cpuAverage();
+
+function cpuAverage() {
+
+    //Initialise sum of idle and time of cores and fetch CPU info
+    var totalIdle = 0, totalTick = 0;
+    var cpus = os.cpus();
+  
+    //Loop through CPU cores
+    for(var i = 0, len = cpus.length; i < len; i++) {
+  
+      //Select CPU core
+      var cpu = cpus[i];
+  
+      //Total up the time in the cores tick
+      for(type in cpu.times) {
+        totalTick += cpu.times[type];
+     }     
+  
+      //Total up the idle time of the core
+      totalIdle += cpu.times.idle;
+    }
+  
+    //Return the average Idle and Tick times
+    return {idle: totalIdle / cpus.length,  total: totalTick / cpus.length};
+  }
+
+var monitorLoop = setInterval(function() {
+    const free_mem = os.freemem();
+    const total_mem = os.totalmem();
+    let used_mem_percent = (total_mem - free_mem)/total_mem;
+
+    //Grab second Measure
+    var endMeasure = cpuAverage(); 
+    //Calculate the difference in idle and total time between the measures
+    var idleDifference = endMeasure.idle - startMeasure.idle;
+    var totalDifference = endMeasure.total - startMeasure.total;
+    //Calculate the average percentage CPU usage
+    var percentageCPU = 100 - ~~(100 * idleDifference / totalDifference);
+    let mem_used_gb = Math.round((total_mem-free_mem)/1024/1024/1024);
+    
+    $(`#cpuPercent`).html(`${percentageCPU}%`);
+    $(`#memUsage`).html(`${mem_used_gb} GB`);
+
+    $(`#ramMeter`).css('height',`${used_mem_percent*100}%`);
+    $(`#cpuMeter`).css('height',`${percentageCPU}%`);
+    
+}, 250);
+
+clearInterval(monitorLoop);
