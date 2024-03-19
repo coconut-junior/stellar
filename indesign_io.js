f12fb0d05d5249373e0526692c4f233ecafe86b4
Jimmy Blanck <blanckjm@gmail.com>
2024-03-19 08:38:40 -0400

    updated icon

--- indesign_io.js
+++ indesign_io.js
@@ -1,12 +1,12 @@
-const { exec } = require("child_process");
-const { gsap } = require("gsap/dist/gsap");
-const dependencyURL = "https://jbx.design/stellar/dependencies.json";
+const { exec } = require('child_process');
+const { gsap } = require('gsap/dist/gsap');
+const dependencyURL = 'https://jbx.design/stellar/dependencies.json';
 const request = require('request');
-const DecompressZip = require("decompress-zip");
+const DecompressZip = require('decompress-zip');
 const party = require('party-js');
-const fs = require("fs");
-const path = require("path");
-const {buildFlyer} = require(path.join(__dirname,"lytho.js"));
+const fs = require('fs');
+const path = require('path');
+const { buildFlyer } = require(path.join(__dirname, 'lytho.js'));
 
 dependencies = [];
 var scriptPath = undefined;
@@ -14,44 +14,48 @@ var scriptPath = undefined;
 getScriptPath();
 
 function makeDir(dir) {
-    ipcRenderer.sendSync('makeDir',dir);
+  ipcRenderer.sendSync('makeDir', dir);
 }
 
 function downloadDependencies() {
-    var i = 0;
+  var i = 0;
 
-    dependencies['scripts'].forEach(function(dependency) {
-        let fileName = dependency['filename'];
-        let scriptName = dependency['name'];
-        let fullScriptName = scriptName;
-        let version = dependency['version'];
-        let hidden = dependency['hidden'];
-        let url = dependency['url'];
-        let filePath = `${scriptPath}/${fileName}`;
-        let description = dependency['description'];
-        let homePath = getHomePath();
+  dependencies['scripts'].forEach(function (dependency) {
+    let fileName = dependency['filename'];
+    let scriptName = dependency['name'];
+    let fullScriptName = scriptName;
+    let version = dependency['version'];
+    let hidden = dependency['hidden'];
+    let url = dependency['url'];
+    let filePath = `${scriptPath}/${fileName}`;
+    let description = dependency['description'];
+    let homePath = getHomePath();
 
-        $(`.statusBar`).html(`Downloading ${fileName}...`);
+    $(`.statusBar`).html(`Downloading ${fileName}...`);
 
-        if(scriptName.length > 24) {
-            scriptName = scriptName.slice(0,20) + "...";
-        }
+    if (scriptName.length > 24) {
+      scriptName = scriptName.slice(0, 20) + '...';
+    }
 
-        if(fileName.match('.zip')) {
-            $(`.statusBar`).html('Creating folders...');
-            let extractPath = path.dirname(`${scriptPath}/${fileName}`);
-            makeDir(extractPath);
-        }
-        
-        $.ajax({
-            url: dependency['url'],
-            success: function(data) {
+    if (fileName.match('.zip')) {
+      $(`.statusBar`).html('Creating folders...');
+      let extractPath = path.dirname(`${scriptPath}/${fileName}`);
+      makeDir(extractPath);
+    }
 
-                if (fs.existsSync(`${scriptPath}/${fileName}`) && !fileName.match('zip')) fs.unlinkSync(`${scriptPath}/${fileName}`);
-                if(!fileName.match('html') && !fileName.match('zip')) fs.writeFileSync(`${scriptPath}/${fileName}`, data);
+    $.ajax({
+      url: dependency['url'],
+      success: function (data) {
+        if (
+          fs.existsSync(`${scriptPath}/${fileName}`) &&
+          !fileName.match('zip')
+        )
+          fs.unlinkSync(`${scriptPath}/${fileName}`);
+        if (!fileName.match('html') && !fileName.match('zip'))
+          fs.writeFileSync(`${scriptPath}/${fileName}`, data);
 
-                if(!hidden) {
-                    let html = `
+        if (!hidden) {
+          let html = `
                     <div class="result" id = "idScript${i}" name = "${scriptName}">
                         <button onclick = "alert('${fullScriptName} \\n\\n ${description}')" class = "navButton navInfo tooltip" style = "background-color:none;border:none;height:30px;width:30px;position:absolute;top:20px;right:20px;background-position:center;">
                             <span class="tooltiptext">Info</span>
@@ -65,214 +69,256 @@ function downloadDependencies() {
                         </div>
                     </div>
                     `;
-                    $(`#automationTasks`).append(html);
+          $(`#automationTasks`).append(html);
 
-                    let id = i;
-                    gsap.from(`#idScript${id}`, {duration: 2, ease: "elastic.out(1, 0.2)",y:-100,opacity:0});
+          let id = i;
+          gsap.from(`#idScript${id}`, {
+            duration: 2,
+            ease: 'elastic.out(1, 0.2)',
+            y: -100,
+            opacity: 0,
+          });
 
-                    $(`#launchButton${id}`).on('click', function(event) {
-                        launch(`#launchButton${id}`, fileName, url, null);
-                    });
+          $(`#launchButton${id}`).on('click', function (event) {
+            launch(`#launchButton${id}`, fileName, url, null);
+          });
 
-                    $(`#automationTasks`).attr('status','none');
-                    ++i;
-                }
-            },
-            error: function(err) {
-                console.log(err.status);
-            }
-        });
-
-        //download and extract zip files
-
-        if(fileName.match('.zip')) {
-            $(`.statusBar`).html('Extracting assets...');
-
-            let out = fs.createWriteStream(`${scriptPath}/${fileName}`);
-            let extractPath = path.dirname(`${scriptPath}${fileName}`);
-
-            request(url).pipe(out).on('finish',function() {
-                console.log(extractPath);
-                let zipPath = `${scriptPath}/${fileName}`;
-                let unzipper = new DecompressZip(zipPath);
-
-                unzipper.on('error', function (err) {
-                    console.log('Caught an error', err);
-                });
-                unzipper.extract({
-                    path: extractPath
-                });
-
-                //build flyer automation
-                console.log('enabling flyer script');
-                $(`#idScript999`).css('content-visibility','visible');
-                $(`#idScript999`).css('background-image', 'none');
-                $(`.statusBar`).html('Automations are up to date.');
-
-            });
-
+          $(`#automationTasks`).attr('status', 'none');
+          ++i;
         }
-
-        ipcRenderer.send('setProgress', i/dependencies['scripts'].length);
-        i = 0;
-        
+      },
+      error: function (err) {
+        console.log(err.status);
+      },
     });
 
-    $(`#buildFlyerInfo`).on('click', function(event) {
-        alert(`Build Flyer \n\n When prompted, select the Feature View spreadsheet that was exported from Badger. Then, wait for the logos to finish downloading. You will be prompted again to select the flyer dimensions.`);
-    });
-    $(`#buildFlyerButton`).on('click', function(event) {
-        launch(`#buildFlyerButton`, 'buildFlyer', null, null);
-    });
-    //
+    //download and extract zip files
 
-    ipcRenderer.send('setProgress', -1);
+    if (fileName.match('.zip')) {
+      $(`.statusBar`).html('Extracting assets...');
+
+      let out = fs.createWriteStream(`${scriptPath}/${fileName}`);
+      let extractPath = path.dirname(`${scriptPath}${fileName}`);
+
+      request(url)
+        .pipe(out)
+        .on('finish', function () {
+          console.log(extractPath);
+          let zipPath = `${scriptPath}/${fileName}`;
+          let unzipper = new DecompressZip(zipPath);
+
+          unzipper.on('error', function (err) {
+            console.log('Caught an error', err);
+          });
+          unzipper.extract({
+            path: extractPath,
+          });
+
+          //build flyer automation
+          console.log('enabling flyer script');
+          $(`#idScript999`).css('content-visibility', 'visible');
+          $(`#idScript999`).css('background-image', 'none');
+          $(`.statusBar`).html('Automations are up to date.');
+        });
+    }
+
+    ipcRenderer.send('setProgress', i / dependencies['scripts'].length);
+    i = 0;
+  });
+
+  $(`#buildFlyerInfo`).on('click', function (event) {
+    alert(
+      `Build Flyer \n\n When prompted, select the Feature View spreadsheet that was exported from Badger. Then, wait for the logos to finish downloading. You will be prompted again to select the flyer dimensions.`
+    );
+  });
+  $(`#buildFlyerButton`).on('click', function (event) {
+    launch(`#buildFlyerButton`, 'buildFlyer', null, null);
+  });
+  //
+
+  ipcRenderer.send('setProgress', -1);
 }
 
 function openDoc(fileName) {
-    shell.openPath(getRelativePath(`${fileName}`));
+  shell.openPath(getRelativePath(`${fileName}`));
 }
 
-function openProductBlock(fileName, indexes){
-    runJSX(`isolate_items.jsx`,`{"${getRelativePath(fileName)}","${indexes}"}`);
+function openProductBlock(fileName, indexes) {
+  runJSX(`isolate_items.jsx`, `{"${getRelativePath(fileName)}","${indexes}"}`);
 }
 
-var launch = function(button, fileName, url, args){
-    repeatCount = 5;
-    gsap.from(button, {duration: 0.5, ease: "circ.in",transformOrigin:"center", scale: 0.9});
-    if(fileName == 'buildFlyer') {
-        gsap.to(button,0.1,{backgroundColor: 'black'});
-    }
+var launch = function (button, fileName, url, args) {
+  repeatCount = 5;
+  gsap.from(button, {
+    duration: 0.5,
+    ease: 'circ.in',
+    transformOrigin: 'center',
+    scale: 0.9,
+  });
+  if (fileName == 'buildFlyer') {
+    gsap.to(button, 0.1, { backgroundColor: 'black' });
+  }
 
-    //button
-    gsap.to(button, {delay: 0.5,duration: 0.2, ease: "circ.out",transformOrigin:"center", scale: 1,
-        onComplete: function() {
-            party.resolvableShapes["star"] = `<img height = "20px" width = "20px" src="images/star.png"/>`;
-            party.sparkles(document.querySelector(button, {
-                spread: 10,
-                count: 10,
-                size: 0.2,
-                speed: 1000,
-                lifetime: party.variation.range(0.5,1),
-                rotation: 0
-            }));
-        }
+  //button
+  gsap.to(button, {
+    delay: 0.5,
+    duration: 0.2,
+    ease: 'circ.out',
+    transformOrigin: 'center',
+    scale: 1,
+    onComplete: function () {
+      party.resolvableShapes[
+        'star'
+      ] = `<img height = "20px" width = "20px" src="images/star.png"/>`;
+      party.sparkles(
+        document.querySelector(button, {
+          spread: 10,
+          count: 10,
+          size: 0.2,
+          speed: 1000,
+          lifetime: party.variation.range(0.5, 1),
+          rotation: 0,
+        })
+      );
+    },
+  });
+
+  if (fileName == 'buildFlyer') {
+    buildFlyer();
+    gsap.to(button, 1, {
+      delay: 1,
+      y: 0,
+      x: 0,
+      backgroundColor: 'var(--primary4)',
+      ease: 'circ.in',
     });
-    
-    if(fileName == 'buildFlyer') {
-        buildFlyer();
-        gsap.to(button,1,{delay: 1,y:0, x:0,backgroundColor:'var(--primary4)',ease: "circ.in",});
-    }
-    else {
-        gsap.to(button,1,{delay: 1,y:0, x:0,ease: "circ.in",
-        onComplete: runTool,
-        onCompleteParams: [fileName, url, args]
+  } else {
+    gsap.to(button, 1, {
+      delay: 1,
+      y: 0,
+      x: 0,
+      ease: 'circ.in',
+      onComplete: runTool,
+      onCompleteParams: [fileName, url, args],
     });
-    }
-
-}
+  }
+};
 
 function getDependencies() {
-    $(`.statusBar`).html('Updating automations...');
-    $.ajax({
-        url: dependencyURL,
-        cache: false,
-        success: function(data, status) {
-            console.log('Connection succeeded.');
-            // dependencies = JSON.parse(data);
-            dependencies = data; //sometimes server responds with json object, otherwise parse json
-            downloadDependencies();
-        },
-        statusCode: {
-            403: function() {
-                showError("Error 403: Tried to reach the database, but the request was blocked.");
-            },
-            404: function() {
-                showError("Error 404: Couldn't reach the database.");
-            },
-        }
-    }).fail(function() {
-        showError('You are offline. Please connect to the internet to access all automations.');
-        $(`.statusBar`).html('You are offline.');
-        $(`#automationTasks`).attr('status','offline');
-    });
+  $(`.statusBar`).html('Updating automations...');
+  $.ajax({
+    url: dependencyURL,
+    cache: false,
+    success: function (data, status) {
+      console.log('Connection succeeded.');
+      // dependencies = JSON.parse(data);
+      dependencies = data; //sometimes server responds with json object, otherwise parse json
+      downloadDependencies();
+    },
+    statusCode: {
+      403: function () {
+        showError(
+          'Error 403: Tried to reach the database, but the request was blocked.'
+        );
+      },
+      404: function () {
+        showError("Error 404: Couldn't reach the database.");
+      },
+    },
+  }).fail(function () {
+    showError(
+      'You are offline. Please connect to the internet to access all automations.'
+    );
+    $(`.statusBar`).html('You are offline.');
+    $(`#automationTasks`).attr('status', 'offline');
+  });
 }
 
 async function getScriptPath() {
-    $(`.statusBar`).html('Checking InDesign configuration...');
-    let versionCmd = 'ls -la /Applications/ | grep InDesign';
-    
-    exec(versionCmd, (error, stdout, stderr) => {
-        var path = getHomePath() + `/Library/Preferences/Adobe InDesign`;
+  $(`.statusBar`).html('Checking InDesign configuration...');
+  let versionCmd = 'ls -la /Applications/ | grep InDesign';
 
-        if (error) {
-            console.log(`error: ${error.message}`);
-            showError(`Couldn't retrieve latest InDesign version. Please reinstall InDesign and try running the software again.`);
-            return;
-        }
-        if (stderr) {
-            showError(`stderr: ${stderr}`);
-            return;
-        }
+  exec(versionCmd, (error, stdout, stderr) => {
+    var path = getHomePath() + `/Library/Preferences/Adobe InDesign`;
 
-        let versions = stdout.split('\n');
-        versions = versions.filter(function(entry) { return /\S/.test(entry); });
-        for(let i = 0;i<versions.length;++i) {
-            versions[i] = parseInt(versions[i].split('Adobe')[1].replace(/^\D+/g, '').replaceAll(':',''));
-        }
-        
-        const max = versions.reduce((a, b) => Math.max(a, b), -Infinity);
-        let versionNumber = max - 2005;
-        $(`#indVersion`).html(`${versionNumber + 2005} (v ${versionNumber}.0)`);
-        versionNumber = 'Version ' + versionNumber + '.0';
+    if (error) {
+      console.log(`error: ${error.message}`);
+      showError(
+        `Couldn't retrieve latest InDesign version. Please reinstall InDesign and try running the software again.`
+      );
+      return;
+    }
+    if (stderr) {
+      showError(`stderr: ${stderr}`);
+      return;
+    }
 
-        scriptPath = path + '/' + versionNumber + '/en_US/Scripts/Scripts Panel';
-        getDependencies();
+    let versions = stdout.split('\n');
+    versions = versions.filter(function (entry) {
+      return /\S/.test(entry);
     });
+    for (let i = 0; i < versions.length; ++i) {
+      versions[i] = parseInt(
+        versions[i].split('Adobe')[1].replace(/^\D+/g, '').replaceAll(':', '')
+      );
+    }
+
+    const max = versions.reduce((a, b) => Math.max(a, b), -Infinity);
+    let versionNumber = max - 2005;
+    $(`#indVersion`).html(`${versionNumber + 2005} (v ${versionNumber}.0)`);
+    versionNumber = 'Version ' + versionNumber + '.0';
+
+    scriptPath = path + '/' + versionNumber + '/en_US/Scripts/Scripts Panel';
+    getDependencies();
+  });
 }
 
 function runJSX(scriptName, arguments) {
-    //save extendscript files
-    var args = arguments ?? `{"stellar"}`;
-    let homePath = getHomePath();
-    let bashScript = `osascript -e 'tell application id "com.adobe.indesign"\nset args to ${args}\ndo script "${scriptPath}/${scriptName}" language javascript with arguments args\nend tell'`;
-    let script = bashScript;
+  //save extendscript files
+  var args = arguments ?? `{"stellar"}`;
+  let homePath = getHomePath();
+  let bashScript = `osascript -e 'tell application id "com.adobe.indesign"\nset args to ${args}\ndo script "${scriptPath}/${scriptName}" language javascript with arguments args\nend tell'`;
+  let script = bashScript;
 
-    //run bash script
-    exec(`${script}`, (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            if(error.message.includes('authorize')) {
-                showError(`Looks like you're missing automation permissions. Please allow Stellar to control InDesign by granting it permission in Settings on your Mac.`);
-                window.open('https://support.apple.com/en-is/guide/mac-help/mchl07817563/mac');
-            }
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-    });
+  //run bash script
+  exec(`${script}`, (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      if (error.message.includes('authorize')) {
+        showError(
+          `Looks like you're missing automation permissions. Please allow Stellar to control InDesign by granting it permission in Settings on your Mac.`
+        );
+        window.open(
+          'https://support.apple.com/en-is/guide/mac-help/mchl07817563/mac'
+        );
+      }
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
 
-    minimizeApp();
+  minimizeApp();
 }
 
 function runTool(fileName, url, args) {
-    minimizeApp();
-    console.log(`starting tool ${fileName}...`);
-    let extension = fileName.split('.')[1];
+  minimizeApp();
+  console.log(`starting tool ${fileName}...`);
+  let extension = fileName.split('.')[1];
 
-    switch(extension) {
-        case 'jsx':
-            console.log('this is an indesign script');
-            runJSX(fileName, args);
-            break;
-        case 'html':
-            console.log('this is a webpage');
-            window.open(url,'_blank','width=1080px');
-            break;
-        default:
-            console.log('unknown tool type');
-    }
+  switch (extension) {
+    case 'jsx':
+      console.log('this is an indesign script');
+      runJSX(fileName, args);
+      break;
+    case 'html':
+      console.log('this is a webpage');
+      window.open(url, '_blank', 'width=1080px');
+      break;
+    default:
+      console.log('unknown tool type');
+  }
 }
--- renderer.js
+++ renderer.js
@@ -1,8 +1,8 @@
-const {ipcRenderer, webFrame } = require('electron');
-const ViewModes = {normal: 0, compact: 1};
-const Store = require('electron-store');
-const { shell } = require('electron');
-let $ = window.$ = window.jQuery = require('jquery');
+const { ipcRenderer, webFrame } = require("electron");
+const ViewModes = { normal: 0, compact: 1 };
+const Store = require("electron-store");
+const { shell } = require("electron");
+let $ = (window.$ = window.jQuery = require("jquery"));
 
 var viewMode = ViewModes.normal;
 var results = [];
@@ -12,138 +12,112 @@ webFrame.setZoomFactor(0.9);
 //remember settings
 
 var store = new Store();
-$(`#appearanceDropdown`).val(store.get('appearance'));
-$(`#minimizeDropdown`).val(String(store.get('minimizeOnLaunch')));
+$(`#appearanceDropdown`).val(store.get("appearance"));
+$(`#minimizeDropdown`).val(String(store.get("minimizeOnLaunch")));
 
 updateAppearance();
 
 function updateAppearance() {
-    let appearance = $(`#appearanceDropdown`).val();
-    if(appearance == "gundam") {
-        console.log('gundam mode!');
-        $(`:root`).attr('theme', 'gundam');
-    }
-    else {
-        $(`:root`).attr('theme', appearance);
-    }
-    setAppearance(appearance);
+  let appearance = $(`#appearanceDropdown`).val();
+  if (appearance == "gundam") {
+    console.log("gundam mode!");
+    $(`:root`).attr("theme", "gundam");
+  } else {
+    $(`:root`).attr("theme", appearance);
+  }
+  setAppearance(appearance);
 }
 
-$(`#appearanceDropdown`).on('change', function(){
-    updateAppearance();
+$(`#appearanceDropdown`).on("change", function () {
+  updateAppearance();
 });
 
-$(`#minimizeDropdown`).on('change', function(){
-    ipcRenderer.sendSync('setMinimizeBehavior',($(`#minimizeDropdown`).val() === "true"));
+$(`#minimizeDropdown`).on("change", function () {
+  ipcRenderer.sendSync(
+    "setMinimizeBehavior",
+    $(`#minimizeDropdown`).val() === "true"
+  );
 });
 
 function createTitleBar() {
-    var windowTopBar = document.getElementById('titlebar')
-    windowTopBar.style.width = "100%"
-    windowTopBar.style.height = "96px"
-    windowTopBar.style.backgroundColor = "transparent"
-    windowTopBar.style.position = "absolute"
-    windowTopBar.style.top = windowTopBar.style.left = 0
-    windowTopBar.style.webkitAppRegion = "drag"
-    windowTopBar.style.zIndex = -1
+  var windowTopBar = document.getElementById("titlebar");
+  windowTopBar.style.width = "100%";
+  windowTopBar.style.height = "96px";
+  windowTopBar.style.backgroundColor = "transparent";
+  windowTopBar.style.position = "absolute";
+  windowTopBar.style.top = windowTopBar.style.left = 0;
+  windowTopBar.style.webkitAppRegion = "drag";
+  windowTopBar.style.zIndex = -1;
 }
 
 function setAppearance(appearance) {
-    ipcRenderer.sendSync('setAppearance',appearance);
+  ipcRenderer.sendSync("setAppearance", appearance);
 }
 
 function showAbout() {
-    ipcRenderer.invoke('showAbout');
+  ipcRenderer.invoke("showAbout");
 }
 function focusWindow() {
-    ipcRenderer.invoke('focusWindow');
+  ipcRenderer.invoke("focusWindow");
 }
 
 function getHomePath() {
-    return ipcRenderer.sendSync('getHome');
+  return ipcRenderer.sendSync("getHome");
 }
 
 function showError(message) {
-    ipcRenderer.sendSync('showError',message);
+  ipcRenderer.sendSync("showError", message);
 }
 
 function minimizeApp() {
-    ipcRenderer.invoke('minimize');
-}
-
-function toggleView() {
-    if(viewMode == ViewModes.normal) {
-        viewMode = ViewModes.compact;
-        viewCompact();
-    }
-    else {
-        viewMode = ViewModes.normal;
-        viewNormal();
-    }
-}
-
-function viewCompact() {
-    $(`.productInfo`).css('flex-direction','row');
-    $(`.productInfo`).css('gap','10px');
-    $(`.resultButtons`).css('display','none');
-    $(`.toggleSwitch`).css('left',"5px");
-    $(`.toggleSwitch`).css('background-image',"url('images/compactview_icon.svg')");
-    $(`hr`).css('border','0');
-    $(`hr`).css('border-right','2px dotted lightgray');
-    $(`hr`).css('margin','0px 30px 0px 0px');
-}
-
-function viewNormal() {
-    $(`.productInfo`).css('flex-direction','column');
-    $(`.productInfo`).css('gap','0');
-    $(`.resultButtons`).css('display','flex');
-    $(`.toggleSwitch`).css('left',"55px");
-    $(`.toggleSwitch`).css('background-image',"url('images/normalview_icon.svg')");
-    $(`hr`).css('border','0');
-    $(`hr`).css('border-top','2px dotted lightgray');
-    $(`hr`).css('margin','10px 0px 10px 0px');
+  ipcRenderer.invoke("minimize");
 }
 
 function getRelativePath(path) {
-    if(path.match('Users')) {
-        var newPath = path.split('/');
-        newPath.splice(0,3);
-        return getHomePath() + '/' + newPath.join('/');
-    }
-    else {
-        return path;
-    }
+  if (path.match("Users")) {
+    var newPath = path.split("/");
+    newPath.splice(0, 3);
+    return getHomePath() + "/" + newPath.join("/");
+  } else {
+    return path;
+  }
 }
 
-$(`#automationSearch`).on('keyup', function(e) {
-    try{search(e.target.value);}
-    catch(e) {console.log('Could not search');}
+$(`#automationSearch`).on("keyup", function (e) {
+  try {
+    search(e.target.value);
+  } catch (e) {
+    console.log("Could not search");
+  }
 });
 
 function search(query) {
-    let resultCount = 0;
+  let resultCount = 0;
 
-    for(let i = 0;i<results.length;++i) {
-        query = query.toLowerCase();
-        let element = results[i];
-        let name = element.getAttribute('name').toLowerCase();
+  for (let i = 0; i < results.length; ++i) {
+    query = query.toLowerCase();
+    let element = results[i];
+    let name = element.getAttribute("name").toLowerCase();
 
-        if(name.match(query)) {
-            element.style.display = "grid";
-            ++resultCount;
-        }
-        else {
-            element.style.display = "none";
-        }
+    if (name.match(query)) {
+      element.style.display = "grid";
+      ++resultCount;
+    } else {
+      element.style.display = "none";
     }
+  }
 
-    if(resultCount < 3) {
-        $(`#automationTasks`).css('grid-template-columns','repeat(auto-fit, minmax(300px, 350px))');
-    }
-    else {
-        $(`#automationTasks`).css('grid-template-columns','repeat(auto-fit, minmax(300px, auto))');
-    }
-
+  if (resultCount < 3) {
+    $(`#automationTasks`).css(
+      "grid-template-columns",
+      "repeat(auto-fit, minmax(300px, 350px))"
+    );
+  } else {
+    $(`#automationTasks`).css(
+      "grid-template-columns",
+      "repeat(auto-fit, minmax(300px, auto))"
+    );
+  }
 }
 
 createTitleBar();
