4a9861c10d5677c67b35d36ddd2cd043720f55bf
Jimmy Blanck <blanckjm@gmail.com>
2026-09-11 10:29:04 -0400

    indesign info accessible from frontend

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,19 +1,23 @@
 use std::fs;
+use serde::{Serialize};
 
 #[tauri::command]
 fn run_script(filename: String) -> Result<String, String> {
   println!("I was invoked from JavaScript, with this message: {}", filename);
-
-  let path = get_script_path()?;
-  println!("script path: {}", path);
-
-  Ok(path)
+  Ok("successful run".to_string())
 }
 
-fn get_script_path() -> Result<String, String> {
+#[derive(Serialize)]
+struct InDesign {
+  version: i32,
+  year: i32,
+  script_path: String
+}
+
+#[tauri::command]
+fn get_id_info() -> Result<InDesign, String> {
     let home = std::env::var("HOME").map_err(|_| "Unable to resolve HOME directory")?;
     let id_path = format!("{home}/Library/Preferences/Adobe InDesign");
-
     let mut versions = Vec::new();
 
     let apps = fs::read_dir("/Applications")
@@ -52,14 +56,21 @@ fn get_script_path() -> Result<String, String> {
     let max = versions.iter().copied().max().unwrap();
     let version_number = max - 2005;
     let version_string = format!("Version {}.0", version_number);
+    let script_path = format!("{id_path}/{version_string}/en_US/Scripts/Scripts Panel");
 
-    Ok(format!("{id_path}/{version_string}/en_US/Scripts/Scripts Panel"))
+    Ok(
+      InDesign {
+        version: version_number,
+        year: max,
+        script_path: script_path
+      }
+    )
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![run_script])
+    .invoke_handler(tauri::generate_handler![run_script, get_id_info])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,19 @@
    import { Button } from "$lib/components/ui/button";
    import { Input } from "$lib/components/ui/input";
    import { invoke } from '@tauri-apps/api/tauri';
+   import { onMount } from 'svelte';
 
-   let name: string = '';
-   let inputValue: string;
-   let submitError = '';
+   type InDesign = {
+      version: number;
+      year: number;
+      script_path: string;
+   };
+
+   let indesign: InDesign;
+
+   onMount(async () => {
+      indesign = await invoke<InDesign>('get_id_info');
+   });
 
    async function runScript() {
       try {
@@ -13,17 +22,15 @@
          console.log('script path:', result);
       } catch (err) {
          console.error('run_script failed:', err);
-         submitError = err instanceof Error ? err.message : String(err);
       }
    }
 </script>
 
 <main class="grid place-items-center h-full w-full bg-neutral-950">
    <section class="flex flex-col justify-center rounded-lg space-y-3 p-6 bg-neutral-900 w-[30rem]">
-      <Input bind:value={inputValue} placeholder="Your name"/>
       <Button on:click={runScript}>Submit</Button>
-      {#if submitError}
-         <p class="text-sm text-red-400">{submitError}</p>
+      {#if indesign}
+         <pre class="text-sm text-green-300">{JSON.stringify(indesign, null, 2)}</pre>
       {/if}
 
    </section>
